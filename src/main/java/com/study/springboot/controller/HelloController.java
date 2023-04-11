2a70ada8174ef317578922fde6396c2109569f4d
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-04-11 23:01:34 +0900

    @PostMapping

--- src/main/java/com/study/springboot/controller/HelloController.java
+++ src/main/java/com/study/springboot/controller/HelloController.java
@@ -1,6 +1,9 @@
 package com.study.springboot.controller;
 
+import com.study.springboot.dto.MemberDto;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/v1/get-api")
@@ -29,6 +32,28 @@ public class HelloController {
     @GetMapping(value = "/variable2/{variable}")
     public String getVariable2(@PathVariable("variable") String var){
         return var;
+    }
+
+    @GetMapping(value = "/request1")
+    public String getRequestParam1(
+            @RequestParam String name,
+            @RequestParam String email,
+            @RequestParam String organization
+    ){
+        return name + " " + email + " " + organization;
+    }
+
+    @GetMapping(value = "/request2")
+    public String getRequestParam2(@RequestParam Map<String, String> param){
+        StringBuilder sb = new StringBuilder();
+        param.forEach((key, value) -> sb.append(key + " : " + value + "\n"));
+        return sb.toString();
+    }
+
+    @GetMapping(value = "/request3")
+    public String getRequestParam3(
+            MemberDto memberDto){
+        return memberDto.toString();
     }
 
 }
